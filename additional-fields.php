a736e433e93676cf81dfbd13100f3adfc94835a7
Adam Ankeli <ankeliadam@gmail.com>
2020-10-10 11:24:35 +0200

    additional field when add more button gets triggered

---
+++ additional-fields.php
@@ -0,0 +1,14 @@
+   <div class="contact-field-sub">
+        <div id="custom-input-container">
+            <div class="input-row">
+                <select name="custom_name[]" class="cprimary-field form-control">
+                    <option value="volvo">Date of Birth</option>
+                    <option value="saab">Gender</option>
+                    <option value="mercedes">Car Make</option>
+                    <option value="audi">Car Colour</option>
+                </select>
+                <input type="text" class="cprimary-field form-control" name="custom_value[]" />
+            </div>
+        </div>
+    </div>
+    
