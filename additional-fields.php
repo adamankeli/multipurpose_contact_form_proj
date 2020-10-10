eaab7e77171c81fad5935d0e9c28ef8bb2431046
Adam Ankeli <ankeliadam@gmail.com>
2020-10-10 11:27:35 +0200

    Js additional fields

--- additional-fields.php
+++ additional-fields.php
@@ -4,7 +4,7 @@
                     <option value="saab">Gender</option>
                     <option value="mercedes">Car Make</option>
                     <option value="audi">Car Colour</option>
-                </select>
+                </select>    
                 <input type="text" class="cprimary-field form-control" name="custom_value[]" />
             </div>
         </div>
