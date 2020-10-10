8099a37f4ad011bd3203679ca34bf489d3d13b93
Adam Ankeli <ankeliadam@gmail.com>
2020-10-10 16:57:24 +0200

    changed values for additional fields

--- additional-fields.php
+++ additional-fields.php
@@ -1,9 +1,9 @@
    <div class="contact-field-sub">
                 <select name="custom_name[]" id="selectFields" class="cprimary-field form-control">
-                    <option value="volvo">Date of Birth</option>
-                    <option value="saab">Gender</option>
-                    <option value="mercedes">Car Make</option>
-                    <option value="audi">Car Colour</option>
+                    <option value="date_of_birth">Date of Birth</option>
+                    <option value="gender">Gender</option>
+                    <option value="car_make">Car Make</option>
+                    <option value="car_colour">Car Colour</option>
                 </select>
                 <input type="text" id="valueFields" class="cprimary-field form-control" name="custom_value[]" />
             </div>
