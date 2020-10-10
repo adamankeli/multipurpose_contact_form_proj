00a00e320cabc094132ea3add251505392840fff
Adam Ankeli <ankeliadam@gmail.com>
2020-10-10 12:56:47 +0200

    functions to validate and insert into db

--- config/config.php
+++ config/config.php
@@ -15,6 +15,51 @@ class Config{
 		}
     } 
 
+    public function htmlvalidation($form_data){
+        $form_data = mysqli_real_escape_string($this->con, trim(strip_tags($form_data)));
+        $form_data = trim( stripslashes( htmlspecialchars( $form_data ) ) );
+        return $form_data;
+    }  
+    
+    public function alter($tblname, $column_name){
+        $query= "SELECT $column_name FROM $tblname";
+        $insert_fire = mysqli_query($this->con, $query);
+        if ($insert_fire){
+            //my_column exists in my_table
+            return $column_name;
+        }
+        else{
+            //my_column doesn't exist in my_table
+            $query= "ALTER TABLE $tblname ADD $column_name VARCHAR(50) NULL";
+            $insert_fire = mysqli_query($this->con, $query);
+        }
+
+        if($insert_fire){
+            return $insert_fire;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function insert($tblname, $filed_data){
+        $query_data = "";
+        foreach ($filed_data as $q_key => $q_value) {
+            $query_data = $query_data."$q_key='$q_value',";
+        }
+
+        $query_data = rtrim($query_data,",");
+        $query = "INSERT INTO $tblname SET $query_data";
+        $insert_fire = mysqli_query($this->con, $query);
+
+        if($insert_fire){
+            return $insert_fire;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
 
 ?>
